01fccfc4c17b76f4feb5c918c9064bbf99edac17
rmur <romuald.murzakov@gmail.com>
2018-06-14 00:25:34 +0100

    Edit Customers and Movies complete

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,8 +11,11 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public Genre Genre { get; set; }
+        [Display (Name="Release Date")]
         public DateTime ReleaseData { get; set; }
         public DateTime DateAdded { get; set; }
         public int NumbersInStock { get; set; }
+        public int Genre_Id { get; set; }
+
     }
 }
